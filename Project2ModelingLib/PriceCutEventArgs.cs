3a7cd4fe5e05538ddf78a731a997e5d013e75a0e
beaver@asu.edu <beaver@asu.edu>
2012-02-16 22:06:13 -0700

    adding event args for price cut

---
+++ Project2ModelingLib/PriceCutEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project2ModelingLib
+{
+    public class PriceCutEventArgs : EventArgs
+    {
+        public double Price { get; set; }
+
+        public PriceCutEventArgs(double price)
+        {
+            Price = price;
+        }
+    }
+}
